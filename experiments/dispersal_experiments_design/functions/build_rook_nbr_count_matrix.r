48f213d348ad5594dde0c2c1a9f39f3b0ea95d86
SamHari1 <samuelharimanana@gmail.com>
2026-05-25 16:28:57 -0400

    Added comments and return statements

--- experiments/dispersal_experiments_design/functions/build_rook_nbr_count_matrix.r
+++ experiments/dispersal_experiments_design/functions/build_rook_nbr_count_matrix.r
@@ -4,10 +4,13 @@
 # Interior cells = 4, edge cells = 3, corner cells = 2.
 
 build_rook_nbr_count_matrix <- function(n_row, n_col) {
+  # start every cell at 4 (N, S, W, E all available for interior cells)
   m          <- matrix(4L, nrow = n_row, ncol = n_col)
+  # top and bottom rows have no northern / southern neighbour respectively
   m[1, ]     <- m[1, ]     - 1L
   m[n_row, ] <- m[n_row, ] - 1L
+  # left and right columns have no western / eastern neighbour respectively
   m[, 1]     <- m[, 1]     - 1L
   m[, n_col] <- m[, n_col] - 1L
-  m
+  return(m)
 }
