23fc186e0220a04010df752549c71dc7d83e8af5
SamHari1 <samuelharimanana@gmail.com>
2026-05-26 09:30:57 -0400

    Cleaned function names

--- experiments/dispersal_experiments_design/functions/enforce_boundary_conditions.r
+++ experiments/dispersal_experiments_design/functions/enforce_boundary_conditions.r
@@ -1,9 +1,9 @@
-# build_rook_nbr_count_matrix.r
+# enforce_boundary_conditions.r
 # Returns an n_row x n_col integer matrix where each value is the number of
 # rook-adjacent (shared-boundary) neighbours for that cell.
 # Interior cells = 4, edge cells = 3, corner cells = 2.
 
-build_rook_nbr_count_matrix <- function(n_row, n_col) {
+enforce_boundary_conditions <- function(n_row, n_col) {
   # start every cell at 4 (N, S, W, E all available for interior cells)
   m          <- matrix(4L, nrow = n_row, ncol = n_col)
   # top and bottom rows have no northern / southern neighbour respectively
